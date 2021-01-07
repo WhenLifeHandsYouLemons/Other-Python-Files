6e0650f9d61bb9a90a88d070de1e338838009fa0
Sooraj Sannabhadti <You'veGotToMakeLemonade@users.noreply.github.com>
2021-01-07 11:31:49 +0800

    Update Theme Park Field Trip Cost Calculator.py

--- Theme Park Field Trip Cost Calculator.py
+++ Theme Park Field Trip Cost Calculator.py
@@ -1,21 +1,15 @@
 """
 For IGCSE Computer Science course
+Made by: Sooraj Sannabhadti
 """
+import time
 
+# Task 1
 cost_of_coach = 550
 entry_cost = 30
 free_tickets = 0
-name_list_count = 0
-paid_list_count = 0
-check_list_count = 0
-profit_or_loss_text = ""
-paid = 0
-
-names_of_students = []
-paid_or_not = []
 
 valid_input = True
-
 while valid_input:
     try:
         no_of_students = int(input("How many students are going on this trip? "))
@@ -32,21 +26,57 @@ while valid_input:
         print("Please input a number.")
         print("")
 
-if no_of_students >= 10:
-    free_tickets = no_of_students // 10
+if no_of_students >= 11:
+    free_tickets = no_of_students // 11
 
-cost_per_student = round(((cost_of_coach / (no_of_students - free_tickets)) + entry_cost), 2)
+cost_of_coach_per_student = round((cost_of_coach / no_of_students), 2)
+cost_of_entry_per_student = round(((entry_cost * (no_of_students - (free_tickets))) / no_of_students), 2)
+cost_per_student = round((cost_of_coach_per_student + cost_of_entry_per_student), 2)
+
 print("")
 print("----------------------------------------")
+print(f"The estimated cost of coach per student will be ${cost_of_coach_per_student}")
+print(f"The estimated cost of entry per student will be ${cost_of_entry_per_student}")
 print(f"The estimated cost per student will be ${cost_per_student}")
 print("----------------------------------------")
 print("")
+
+# Task 3
+time.sleep(2.5)
+valid_input = True
+while valid_input:
+    try:
+        no_of_students = int(input("How many students are STILL going on this trip? "))
+
+        if no_of_students < 0:
+            print("Please input a valid, positive number between 0 and 45")
+            print("")
+        elif no_of_students > 45:
+            print("Sorry, the maximum number of students allowed on this trip is 45.")
+            print("")
+        else:
+            print("")
+            valid_input = False
+    except ValueError:
+        print("Please input a number.")
+        print("")
+
+
+# Task 2
+name_list_count = 0
+paid_list_count = 0
+check_list_count = 0
+profit_or_loss_text = ""
+paid = 0
+
+names_of_students = []
+paid_or_not = []
 
 while no_of_students != 0:
     name_of_person = input(f"What is the first name of the student number {no_of_students}? ")
     print("")
 
-    if name_of_person.isalpha():
+    if name_of_person != "":
         names_of_students.append(name_of_person)
         no_of_students = no_of_students - 1
 
@@ -71,7 +101,7 @@ while no_of_students != 0:
                 print("Please input 'paid' or 'not paid'")
                 print("")
     else:
-        print("Please type the first name of a student only.")
+        print("Please type the name of a student only.")
         print("")
 
 valid_input = len(names_of_students)
@@ -86,8 +116,7 @@ while valid_input != 0:
     paid_list_count = paid_list_count + 1
     valid_input = valid_input - 1
 
-cost_charged = cost_of_coach + (len(names_of_students) * entry_cost)
-
+# Task 3
 valid_input = len(paid_or_not)
 
 while valid_input != check_list_count:
@@ -97,13 +126,19 @@ while valid_input != check_list_count:
     else:
         check_list_count = check_list_count + 1
 
-money_collected = paid * cost_per_student
-profit_or_loss = round(money_collected - cost_charged, 2)
+if paid >= 11:
+    free_tickets = paid // 11
+    print(f"The free tickets are {free_tickets}, the paid are {paid}")
+# cost_of_coach_per_student = round((cost_of_coach / paid), 2)
+# cost_of_entry_per_student = round(((entry_cost * (paid - (free_tickets))) / paid), 2)
+cost_charged = round((cost_of_coach + ((paid - free_tickets) * entry_cost)), 2)
+
+money_collected = round((paid * cost_per_student), 2)
+profit_or_loss = round((money_collected - cost_charged), 2)
 
 if profit_or_loss > 0:
     profit_or_loss_text = "made a profit"
 elif profit_or_loss < 0:
-    profit_or_loss = profit_or_loss * -1
     profit_or_loss_text = "made a loss"
 else:
     profit_or_loss_text = "broken even"
@@ -113,3 +148,85 @@ print(f"The total money charged is ${cost_charged}")
 print(f"The total money collected is ${money_collected}.")
 print(f"The trip has {profit_or_loss_text} by ${profit_or_loss}.")
 print("----------------------------------------")
+
+"""
+TEST DATA #1
+15
+20
+a
+paid
+b
+paid
+c
+paid
+d
+paid
+e
+paid
+f
+paid
+f
+paid
+g
+paid
+h
+paid
+i
+paid
+j
+paid
+k
+paid
+l
+paid
+m
+paid
+n
+paid
+o
+paid
+p
+paid
+q
+paid
+r
+paid
+s
+paid
+
+
+TEST DATA #2
+20
+15
+a
+paid
+b
+paid
+c
+paid
+d
+paid
+e
+paid
+f
+paid
+f
+paid
+g
+paid
+h
+paid
+i
+paid
+j
+paid
+k
+paid
+l
+paid
+m
+paid
+n
+paid
+
+"""
