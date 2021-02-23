8bfe6ea60042e4603d94ad483b4b5de96e0a91f6
Sooraj Sannabhadti <You'veGotToMakeLemonade@users.noreply.github.com>
2021-02-23 17:13:55 +0800

    Create Electric Mountain Trains.py

---
+++ Electric Mountain Trains.py
@@ -0,0 +1,21 @@
+"""
+For IGCSE Computer Science course & IGCSE Computer Science Exam
+Made by: Sooraj Sannabhadti
+"""
+
+# Task 1
+passengers_per_train = []
+money_per_train = []
+
+# Task 2
+
+print(""" Screen display
+--------------------------------------------------------------
+Departure times	        Seats   |	Return times	Seats
+    09:00		 480	|	    10:00	 480
+    11:00		 480	|	    12:00	 480
+    13:00		 480	|	    14:00	 480
+    15:00		 480	|	    16:00	 640
+--------------------------------------------------------------""")
+
+
